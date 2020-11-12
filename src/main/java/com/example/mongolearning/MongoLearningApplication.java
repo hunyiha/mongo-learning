d193c9700f5cdb7e2771cc5f57b729d72f5ab0f2
hunyiha <hunyiha@gmail.com>
2020-11-13 01:12:11 +0800

    mongo demo

---
+++ src/main/java/com/example/mongolearning/MongoLearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.mongolearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoLearningApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/mongolearning/pojo/Status.java
@@ -0,0 +1,14 @@
+package com.example.mongolearning.pojo;
+
+import lombok.Data;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+@Data
+@ToString
+@Accessors(chain = true)
+public class Status {
+
+    private Integer weight;
+    private Integer height;
+}
